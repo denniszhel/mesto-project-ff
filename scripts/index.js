2515a9daf80a416da52228d38960b468e96ac1a3
Dennis Zheludev <denzh2015@yandex.ru>
2024-04-08 21:17:01 +0300

    feature. complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
+let currentCard;
+let cardElem;
+let cardImage;
+
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template");
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function getCardElem(cardObj, deleteFunction) {
+  if ("name" in cardObj && "link" in cardObj) {
+    cardElem = cardTemplate.content.cloneNode(true);
+    cardImage = cardElem.querySelector(".card__image");
+    cardImage.setAttribute("src", cardObj.link);
+    cardImage.setAttribute("alt", cardObj.name);
+    cardElem.querySelector(".card__title").textContent = cardObj.name; 
+    cardElem.querySelector(".card__delete-button").addEventListener('click', deleteFunction);
+  } else {
+    cardElem = undefined;
+  }
+  return cardElem;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+  event.target.parentNode.remove();  
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardObj) => {
+  currentCard = getCardElem(cardObj, deleteCard);
+  if (currentCard) {
+    placesList.append(cardElem);
+  }
+});
