81f79332ec713a1e98b456a0e7c65e8c725883f0
Dennis Zheludev <denzh2015@yandex.ru>
2024-04-09 10:28:12 +0300

    fix. first review errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,3 @@
-let currentCard;
-let cardElem;
-let cardImage;
-
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template");
 
@@ -10,28 +6,27 @@ const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function getCardElem(cardObj, deleteFunction) {
+  let cardElem;
   if ("name" in cardObj && "link" in cardObj) {
     cardElem = cardTemplate.content.cloneNode(true);
-    cardImage = cardElem.querySelector(".card__image");
+    const cardImage = cardElem.querySelector(".card__image");
     cardImage.setAttribute("src", cardObj.link);
     cardImage.setAttribute("alt", cardObj.name);
     cardElem.querySelector(".card__title").textContent = cardObj.name; 
-    cardElem.querySelector(".card__delete-button").addEventListener('click', deleteFunction);
-  } else {
-    cardElem = undefined;
+    cardElem.querySelector(".card__delete-button").addEventListener("click", deleteFunction);
   }
   return cardElem;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard() {
-  event.target.parentNode.remove();  
+function deleteCard(evt) {
+  evt.target.closest(".places__item").remove();  
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardObj) => {
-  currentCard = getCardElem(cardObj, deleteCard);
+  const currentCard = getCardElem(cardObj, deleteCard);
   if (currentCard) {
-    placesList.append(cardElem);
+    placesList.append(currentCard);
   }
 });
