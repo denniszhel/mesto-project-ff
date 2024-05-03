5ed853e8bedb9dc373d3a2e882b4a1367d23e245
Dennis Zheludev <denzh2015@yandex.ru>
2024-05-03 20:22:31 +0300

    fix. first review errors 2

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,3 @@
-import { Promise } from "core-js";
 import { checkResponse } from "./utils";
 
 const config = {
--- src/components/validation.js
+++ src/components/validation.js
@@ -1,5 +1,3 @@
-// const urlPattern = new RegExp(/^(?:(?:https?|ftp|telnet):\/\/(?:[a-z0-9_-]{1,32}(?::[a-z0-9_-]{1,32})?@)?)?(?:(?:[a-z0-9-]{1,128}\.)+(?:com|net|org|mil|edu|arpa|ru|gov|biz|info|aero|inc|name|[a-z]{2})|(?!0)(?:(?!0[^.]|255)[0-9]{1,3}\.){3}(?!0|255)[0-9]{1,3})(?:\/[a-z0-9.,_@%&?+=\~\/-]*)?(?:#[^ \'\"&<>]*)?$/i);
-
 function showInputError(formElement, inputElement, validationConfig) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(validationConfig.inputErrorClass);
